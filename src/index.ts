3ece10dcec22c0653f04a7d41e2e21083849b375
Michael E Adebisi <adebisienads@gmail.com>
2022-07-29 11:05:19 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,13 @@ import {
 import log from './utils/logger';
 import { connectToMongoDb } from './utils/mongo';
 import { ContactResolver } from './contact/resolver/contact.resolver';
+import cors from 'cors'
 
 dotenv.config();
 const main = async () => {
   const string = process.env.MONGO_CONNECTION_STRING as string;
   const DB = string.replace('<PASSWORD>', process.env.DB_PASSWORD as string);
-  const PORT = process.env.PORT
+  const PORT = process.env.PORT;
 
   const schema = await buildSchema({
     resolvers: [ContactResolver],
@@ -25,11 +26,43 @@ const main = async () => {
     validate: false,
   });
 
+  const whitelist = [
+    process.env.CLIENT_URL,
+    process.env.ORIGIN_SERVER,
+    process.env.RDC_CLIENT_URL,
+  ];
+
+  const corsOptions = {
+    origin(
+      origin: string | undefined,
+      callback: (arg0: Error | null, arg1: boolean | undefined) => void
+    ) {
+      if (
+        origin === undefined ||
+        whitelist.indexOf(origin) !== -1 ||
+        /^https:\/\/(.*\.)?zwilt.com$/.test(origin) ||
+        (process.env.NODE_ENV !== 'production' &&
+          /^https?:\/\/localhost:\d{4}$/.test(origin))
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'), false);
+      }
+    },
+    credentials: true,
+  };
+
   const app = express();
 
   app.use(express.json());
-  app.use(cookieParser()); 
-  app.set('trust proxy', 1);
+  app.use(cookieParser());
+  // protecting our api from unauthorized origins
+  app.use(cors(corsOptions));
+
+  // initializing session and httpOnly cookies
+  if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1);
+  }
 
   const server = new ApolloServer({
     schema,
