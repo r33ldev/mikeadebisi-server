30c7d7d0a60f0fb93c97f3da34ae090a41252625
Michael E Adebisi <adebisienads@gmail.com>
2022-07-29 10:48:05 +0100

    ...

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,8 @@
 import logger from 'pino';
 import dayjs from 'dayjs';
-import config from 'config';
 
-const level = config.get<string>('logLevel');
+
+const level = 'info'
 
 const log = logger({
   transport: {
---
+++ src/utils/mailer.ts
@@ -0,0 +1,3 @@
+export const mailer = async (input: any) => {
+  console.log(input);
+};
